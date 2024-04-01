40a2f976538aba0bd62bf030682c77f8b23df34a
Sokilskill <vprovorotov@gmail.com>
2024-04-01 10:32:52 +0300

    Create Main Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Header } from "../components/Header";
 import { SectionHero } from "../components/SectionHero";
 import { Footer } from "../components/Footer";
+import { SectionAbout } from "../components/SectionAbout";
+import { SectionChoose } from "../components/SectionChoose";
+import { SectionTerms } from "../components/SectionTerms";
+import { SectionLocation } from "../components/SectionLocation";
+import { SectionContacts } from "../components/SectionContacts";
+import { SectionHelp } from "../components/SectionHelp";
 
 const LandingPage: React.FC = () => {
   return (
@@ -9,6 +15,12 @@ const LandingPage: React.FC = () => {
       <Header />
       <main>
         <SectionHero />
+        <SectionAbout />
+        <SectionChoose />
+        <SectionTerms />
+        <SectionLocation />
+        <SectionContacts />
+        <SectionHelp />
       </main>
       <Footer />
     </>
