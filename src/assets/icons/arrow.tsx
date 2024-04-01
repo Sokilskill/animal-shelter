671e226355566869fd5fc5dda72568cbcce3563d
Sokilskill <vprovorotov@gmail.com>
2024-04-01 10:24:20 +0300

    create icon arrow

---
+++ src/assets/icons/arrow.tsx
@@ -0,0 +1,192 @@
+import { Icon, IconProps } from "@chakra-ui/react";
+
+export const ArrowIcon: React.FC<IconProps> = (props) => (
+  <Icon viewBox="0 0 54 54" fill="none" {...props}>
+    <g filter="url(#filter0_d_36213_234)">
+      <circle
+        cx="26"
+        cy="25"
+        r="22"
+        transform="rotate(-90 26 25)"
+        fill="white"
+        fillOpacity="0.4"
+        shapeRendering="crispEdges"
+      />
+      <circle
+        cx="26"
+        cy="25"
+        r="21.5"
+        transform="rotate(-90 26 25)"
+        stroke="url(#paint0_linear_36213_234)"
+        shapeRendering="crispEdges"
+      />
+    </g>
+    <g filter="url(#filter1_ddddd_36213_234)">
+      <line
+        x1="34.3136"
+        y1="24.1872"
+        x2="22.9973"
+        y2="35.5035"
+        stroke="#79A5FF"
+        strokeWidth="2"
+      />
+      <line
+        x1="23.0919"
+        y1="14.204"
+        x2="34.4081"
+        y2="25.5203"
+        stroke="#79A5FF"
+        strokeWidth="2"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_36213_234"
+        x="0"
+        y="0"
+        width="54"
+        height="54"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="1" dy="2" />
+        <feGaussianBlur stdDeviation="2.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0.643137 0 0 0 0 0.760784 0 0 0 0 1 0 0 0 1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_36213_234"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_36213_234"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="filter1_ddddd_36213_234"
+        x="7.68652"
+        y="13.4969"
+        width="31.4287"
+        height="36.8105"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_36213_234"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="1" />
+        <feGaussianBlur stdDeviation="0.5" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect1_dropShadow_36213_234"
+          result="effect2_dropShadow_36213_234"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="2" />
+        <feGaussianBlur stdDeviation="1" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.09 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect2_dropShadow_36213_234"
+          result="effect3_dropShadow_36213_234"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="5" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.05 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect3_dropShadow_36213_234"
+          result="effect4_dropShadow_36213_234"
+        />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="10" />
+        <feGaussianBlur stdDeviation="2" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.01 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="effect4_dropShadow_36213_234"
+          result="effect5_dropShadow_36213_234"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect5_dropShadow_36213_234"
+          result="shape"
+        />
+      </filter>
+      <linearGradient
+        id="paint0_linear_36213_234"
+        x1="20.5"
+        y1="16.4062"
+        x2="26"
+        y2="47"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="white" />
+        <stop offset="1" stopColor="#A4C2FF" stopOpacity="0" />
+      </linearGradient>
+    </defs>
+  </Icon>
+);
