e5a8ba5b639597a27c2b71b4e5ea057f06b2ae2a
Sokilskill <vprovorotov@gmail.com>
2024-05-19 12:36:52 +0300

    update style, add img for desktop

--- src/components/SectionTerms.tsx
+++ src/components/SectionTerms.tsx
@@ -6,53 +6,85 @@ import {
   List,
   ListItem,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import catImg from "../assets/terms/cat-mob.png";
 import bigDogImg from "../assets/terms/big-dog-mob.png";
 import littleDogImg from "../assets/terms/little-dog-mob.png";
 
+import catImgDes from "../assets/terms/cat-des.png";
+import bigDogImgDes from "../assets/terms/big-dog-des.png";
+import littleDogImgDes from "../assets/terms/little-dog-des.png";
+
 const animalsList = [
   {
     title: "Лише власник",
     description: "Собаку чи кота віддаємо лише в руки майбутньому власнику",
-    image: littleDogImg,
+    images: {
+      mob: littleDogImg,
+      md: "",
+      lg: littleDogImgDes,
+    },
   },
   {
     title: "Тварина для сім'ї",
     description:
       "Тваринки не для утримання на ланцюгу, охорони підприєства чи самовигулу",
-    image: catImg,
+    images: { mob: catImg, md: "", lg: catImgDes },
   },
   {
     title: "Відповідальність",
     description:
       "Не забирайте тварину з притулку, якщо не готові про неї піклуватись",
-    image: bigDogImg,
+    images: { mob: bigDogImg, md: "", lg: bigDogImgDes },
   },
 ];
 
 export const SectionTerms = () => {
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
+
   return (
-    <Box as="section" pb="60px">
+    <Box
+      as="section"
+      pb={{ base: "40px", sm: "60px", md: "100px", lg: "185px" }}
+    >
       <Container>
-        <Heading variant="sectionHeading" mb={3}>
+        <Heading variant="sectionHeading" mb={{ base: 3, md: 16 }}>
           Наші умови
         </Heading>
 
-        <List display="flex" flexDirection="column" gap={6}>
+        <List
+          display="flex"
+          flexDirection={{ base: "column", md: "row" }}
+          gap={{ base: 6, md: 2 }}
+          justifyContent={{ md: "space-between" }}
+        >
           {animalsList.map((animal, index) => (
             <ListItem
               key={index}
               display="flex"
-              flexDirection="column"
-              alignItems="center"
+              flexDirection={index !== 1 ? "column" : "column-reverse"}
+              alignItems={{ base: "center" }}
               gap={3}
+              w={{ base: "327px", md: "306px", lg: "425px" }}
+              justifyContent={{ md: "space-between" }}
             >
-              <Heading as="h3" color="custom.blue">
-                {animal.title}
-              </Heading>
-              <Text>{animal.description}</Text>
-              <Image src={animal.image} alt="зображення тваринки" />
+              <Box>
+                <Heading as="h3" variant="subTitle" mb={{ base: 3, md: 4 }}>
+                  {animal.title}
+                </Heading>
+                <Text
+                  fontSize={{ md: "20px", lg: "24px" }}
+                  fontWeight={{ md: 400, lg: 700 }}
+                >
+                  {animal.description}
+                </Text>
+              </Box>
+
+              <Image
+                src={isLargeScreen ? animal.images.lg : animal.images.mob}
+                alt="зображення тваринки"
+              />
             </ListItem>
           ))}
         </List>
