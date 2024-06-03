b242d11a6877bff477290f999033d4ee70076f7b
Sokilskill <vprovorotov@gmail.com>
2024-06-03 15:17:13 +0300

    update Link

--- src/components/NavList/NavList.tsx
+++ src/components/NavList/NavList.tsx
@@ -1,26 +1,29 @@
-import { Link as RouterLink } from "react-router-dom";
-import { List, ListItem, ListProps } from "@chakra-ui/react";
+import { Link, List, ListItem, ListProps } from "@chakra-ui/react";
 import { ListNav } from "../../types";
-
 // =========  список для навігації по сайту ============
 
 export const NavList: React.FC<{
   listItem: ListNav[];
+  onClick?: () => void;
   "aria-label": string;
   style?: ListProps;
-}> = ({ listItem, "aria-label": ariaLabel, style }) => {
+}> = ({ listItem, "aria-label": ariaLabel, style, onClick }) => {
   return (
     <List
       display={"flex"}
       flexDirection="column"
       rowGap={2}
-      w={{ base: "164px" }}
+      w={{ base: "164px", md: "230px", lg: "213px" }}
       aria-label={ariaLabel}
       {...style}
     >
       {listItem.map((item, index) => (
-        <ListItem key={index} fontSize={{ base: "16px" }}>
-          <RouterLink to={`/${item.to}`}>{item.name}</RouterLink>
+        <ListItem
+          key={index}
+          fontSize={{ base: "16px", lg: "20px" }}
+          onClick={onClick}
+        >
+          <Link href={item.to}>{item.name}</Link>
         </ListItem>
       ))}
     </List>
