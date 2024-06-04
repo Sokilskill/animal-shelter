4930078a5a7bbc365017fa1fca0e976093437d65
Sokilskill <vprovorotov@gmail.com>
2024-06-04 21:35:47 +0300

    add lazy loading

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,26 +1,36 @@
-import React from "react";
-import { Header } from "../components/Header";
-import { SectionHero } from "../components/SectionHero";
-import { Footer } from "../components/Footer";
-import { SectionAbout } from "../components/SectionAbout";
-import { SectionChoose } from "../components/SectionChoose";
-import { SectionTerms } from "../components/SectionTerms";
-import { SectionLocation } from "../components/SectionLocation";
-import { SectionContacts } from "../components/SectionContacts";
-import { SectionHelp } from "../components/SectionHelp";
+import React, { lazy, Suspense } from "react";
+import Header from "../components/Header/Header";
+import { Footer } from "../components/Footer/Footer";
+import { SuspenseLoader } from "../components/Loader/SuspenseLoader";
+
+const SectionHero = lazy(() => import("../components/Sections/SectionHero"));
+const SectionAbout = lazy(() => import("../components/Sections/SectionAbout"));
+const SectionChoose = lazy(
+  () => import("../components/Sections/SectionChoose")
+);
+const SectionTerms = lazy(() => import("../components/Sections/SectionTerms"));
+const SectionLocation = lazy(
+  () => import("../components/Sections/SectionLocation")
+);
+const SectionContacts = lazy(
+  () => import("../components/Sections/SectionContacts")
+);
+const SectionHelp = lazy(() => import("../components/Sections/SectionHelp"));
 
 const LandingPage: React.FC = () => {
   return (
     <>
       <Header />
       <main>
-        <SectionHero />
-        <SectionAbout />
-        <SectionChoose />
-        <SectionTerms />
-        <SectionLocation />
-        <SectionContacts />
-        <SectionHelp />
+        <Suspense fallback={<SuspenseLoader />}>
+          <SectionHero />
+          <SectionAbout />
+          <SectionChoose />
+          <SectionTerms />
+          <SectionLocation />
+          <SectionContacts />
+          <SectionHelp />
+        </Suspense>
       </main>
       <Footer />
     </>
