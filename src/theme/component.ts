ea37865fd8cb2f6a6f893408d998d846794a4f06
Sokilskill <vprovorotov@gmail.com>
2024-06-05 13:39:30 +0300

    update styles Button, Heading

--- src/theme/component.ts
+++ src/theme/component.ts
@@ -1,5 +1,4 @@
 // Container
-
 export const Container = {
   baseStyle: {
     margin: "0 auto",
@@ -15,7 +14,6 @@ export const Container = {
     "@media screen and (min-width: 1440px)": {
       maxWidth: "1440px",
       paddingX: "40px",
-      outline: "1px solid yellow",
     },
   },
 };
@@ -30,46 +28,62 @@ export const Button = {
   },
   variants: {
     primary: {
+      fontSize: { base: "28px", sm: "30px", md: "36px" },
+      width: "160px",
       color: "white",
       bgColor: "custom.red",
-      boxShadow: " 0 0 40px 0 #EF395966",
-      borderTopRadius: "25px",
-      borderBottomEndRadius: "25px",
-      borderWidth: "3px",
+      boxShadow: "0 0 40px 0 #EF395966",
+      borderRadius: "25px 25px 25px 0",
+      borderWidth: "none",
       borderColor: "transparent",
-      transition:
-        "background-color 0.4s, border-color 0.4s, color 0.4s, boxShadow 0.4s",
+      transition: "all 200ms linear",
+
       _hover: {
         color: "custom.red",
-        boxShadow: "unset",
+        boxShadow: "none",
+        bgColor: "transparent",
+        borderWidth: { base: "4px" },
         borderColor: "custom.red",
-        bgColor: " transparent",
-        textDecoration: "none",
       },
     },
     secondary: {
-      position: "relative",
-      overflow: "hidden",
-      p: "15px",
-      bgColor: "white",
-      borderWidth: "3px",
-      borderTopRadius: "25px",
-      borderBottomEndRadius: "25px",
-      transition: "background-color 0.4s, border-color 0.4s, color 0.4s",
-      _before: {
-        content: '""',
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: -1,
-        borderRadius: "inherit",
-        backgroundImage:
-          "linear-gradient(to right, #4A86FF, #F5F5F5, #EF395966)",
-      },
+      width: { base: "327px", md: "442px" },
+      height: { base: "80px", md: "102px" },
+      color: "custom.red",
+      fontSize: { base: "20px", sm: "32px", md: "36px" },
+      fontWeight: { md: "600" },
+      lineHeight: { base: 1.6, md: 1.2 },
+      borderRadius: "25px 25px 25px 0",
+      border: "3px solid transparent",
+
+      background: `custom.borderGradient`,
+      backgroundOrigin: "border-box",
+      backgroundClip: "content-box, border-box",
+      transition: "all 200ms linear",
+
       _hover: {
-        borderWidth: ["6px", "6px", "7px"],
+        borderWidth: { base: "6px", md: "7px" },
+      },
+    },
+    help: {
+      fontSize: { base: "15px", sm: "20px", md: "24px" },
+      fontWeight: { base: 400, lg: 600 },
+      width: { base: "160px", md: "213px" },
+      height: { base: "60px", md: "76px" },
+      color: "white",
+      bgColor: "custom.red",
+      boxShadow: "0 0 40px 0 #EF395966",
+      borderRadius: { base: "15px", md: "25px" },
+      borderWidth: "none",
+      borderColor: "transparent",
+      transition: "all 200ms linear",
+
+      _hover: {
+        color: "custom.red",
+        boxShadow: "none",
+        bgColor: "transparent",
+        borderWidth: { base: "2px", md: "4px" },
+        borderColor: "custom.red",
       },
     },
   },
@@ -83,9 +97,25 @@ export const Button = {
 export const Link = {
   baseStyle: {
     display: "block",
-    fontWeight: "normal",
+    fontWeight: "inherit",
     _hover: {
       textDecoration: "none",
+    },
+  },
+  variants: {
+    contactLink: {
+      fontWeight: "500",
+      fontSize: "16px",
+      color: "red",
+
+      "@media screen and (min-width: 1024px)": {
+        fontSize: "24px",
+      },
+
+      "@media screen and (min-width: 1440px)": {
+        fontWeight: "700",
+        fontSize: "30px",
+      },
     },
   },
 };
@@ -93,23 +123,50 @@ export const Link = {
 // Heading
 export const Heading = {
   baseStyle: {
-    lineHeight: 1.2,
+    lineHeight: 1.3,
     fontWeight: "semibold",
     fontSize: "32px",
   },
   sizes: {},
   variants: {
     sectionHeading: {
-      "@media screen and (max-width: 767px)": {
-        textAlign: "center",
+      textAlign: "center",
+
+      "@media screen and (min-width: 1024px)": {
+        fontWeight: "extrabold",
+        fontSize: "56px",
       },
-      "@media screen and (min-width: 768px)": {
-        // color: " red",
+
+      "@media screen and (min-width: 1440px)": {
+        fontWeight: "bold",
+      },
+    },
+    subTitle: {
+      textAlign: "center",
+
+      color: "custom.blue",
+      "@media screen and (min-width: 1024px)": {
+        textAlign: "start",
+      },
+      "@media screen and (min-width: 1440px)": {
+        fontWeight: "bold",
+        fontSize: "40px",
       },
     },
   },
   defaultProps: {
     baseStyle: "",
     size: "",
+  },
+};
+
+export const Text = {
+  baseStyle: {
+    color: "custom.blackTypography",
+  },
+  variants: {
+    contactText: {
+      color: "custom.blue",
+    },
   },
 };
