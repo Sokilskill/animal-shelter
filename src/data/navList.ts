30124046710bddd70313616e010b47087361335f
Sokilskill <vprovorotov@gmail.com>
2024-06-05 13:42:32 +0300

    Added lists of navigation links for different sections and breakpoints

--- src/data/navList.ts
+++ src/data/navList.ts
@@ -1,37 +1,37 @@
-import { ListNav } from '../types';
+import { ListNav } from "../types";
 
 export const listGoodness: ListNav[] = [
-  { name: 'Вірні друзі', to: '#catalog' },
-  { name: 'Обрати друга', to: '#reviews' },
-  { name: 'Щасливчики', to: '#how-to-order' },
-  { name: 'Стати волонтером', to: '#contact' },
+  { name: "Вірні друзі", to: "#catalog" },
+  { name: "Обрати друга", to: "#reviews" },
+  { name: "Щасливчики", to: "#how-to-order" },
+  { name: "Стати волонтером", to: "#contact" },
 ];
 
 export const listAbout: ListNav[] = [
-  { name: 'Про нас', to: '#about-us' },
-  { name: 'Наші умови', to: '#terms' },
-  { name: 'Наша команда', to: '#contact' },
-  { name: 'Історія створення', to: '#contact' },
+  { name: "Про нас", to: "#about-us" },
+  { name: "Наші умови", to: "#terms" },
+  { name: "Наша команда", to: "#contact" },
+  { name: "Історія створення", to: "#contact" },
 ];
 export const listContactDetails: ListNav[] = [
-  { name: 'Локація', to: '#location' },
-  { name: 'Контакти', to: '#contact' },
-  { name: 'Інормація', to: '#info' },
+  { name: "Локація", to: "#location" },
+  { name: "Контакти", to: "#contact" },
+  { name: "Інормація", to: "#info" },
 ];
 
-export const navHeader: ListNav[] = [
-  { name: 'Про нас', to: '#about-us' },
-  { name: 'Вірні друзі', to: '#catalog' },
-  { name: 'Наші умови', to: '#terms' },
-  { name: 'Локація', to: '#location' },
-  { name: 'Контакти', to: '#contact' },
+export const mobileNavigationList: ListNav[] = [
+  { name: "Про нас", to: "#about-us" },
+  { name: "Вірні друзі", to: "#catalog" },
+  { name: "Наші умови", to: "#terms" },
+  { name: "Локація", to: "#location" },
+  { name: "Контакти", to: "#contact" },
 ];
 
 export const navHeaderDesktop: ListNav[] = [
-  { name: 'Домашня', to: '/' },
-  { name: 'Про нас', to: '#about-us' },
-  { name: 'Вірні друзі', to: '#catalog' },
-  { name: 'Наші умови', to: '#terms' },
-  { name: 'Локація', to: '#location' },
-  { name: 'Контакти', to: '#contact' },
+  { name: "Домашня", to: "/" },
+  { name: "Про нас", to: "#about-us" },
+  { name: "Вірні друзі", to: "#catalog" },
+  { name: "Наші умови", to: "#terms" },
+  { name: "Локація", to: "#location" },
+  { name: "Контакти", to: "#contact" },
 ];
